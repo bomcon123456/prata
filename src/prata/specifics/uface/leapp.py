f094231102a97309be1be44c9fba09a9e67ac6e7
v.trungdt21 <v.trungdt21@vinai.io>
2023-04-05 20:11:43 +0700

    feat: gg

--- src/prata/specifics/uface/leapp.py
+++ src/prata/specifics/uface/leapp.py
@@ -1,8 +1,10 @@
 import typer
 from .validate_data import app as ValidateDataApp
+from .longrange import app as LongrangeApp
 from .g3 import app as G3App
 
 app = typer.Typer()
 
 app.add_typer(ValidateDataApp, name="dataval")
+app.add_typer(LongrangeApp, name="longrange")
 app.add_typer(G3App, name="g3")
